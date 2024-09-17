624165affdba042f87366824257136c346e6bb50
Ji <161530087+lilwoot@users.noreply.github.com>
2024-09-17 15:30:11 -0600

    Created and Edit
    Created page. js and student-info.js in the week-2 folder and added some code.
    
    edited the page.js in the app folder to follow the instructions for assignment 2.

---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import React from 'react';
+
+export default function StudentInfo() {
+    return (
+        <main>
+            <h1>Jiyun Woo</h1>
+            <Link href ="https://github.com/WJiyun">Link to My GitHub Profile
+            </Link>
+        </main>
+    );
+}
+
